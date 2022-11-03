d234281b4e723486208f148296ad399aab86df98
arasuraj97 <116246082+arasuraj97@users.noreply.github.com>
2022-11-03 12:06:03 +0530

    Add files via upload
    Day 3

---
+++ Scr.js
@@ -0,0 +1,71 @@
+var json={
+
+    jsondata:{
+        one:[11,12,13,14,15],
+        two:[21,22,23],
+        Three:[31,32]
+    }
+};
+for(var key in json.jsondata ){
+    for(var key1 in json.jsondata[key]){
+        console.log([key]);
+    }
+}
+
+
+// _____________************************____________________________----
+
+// const resume=[
+//     {
+//         "name":"arasu",
+//         "qual":"be"
+//     },
+//     {
+//         "native":"Erode",
+//         "workplace":"chennai"
+//     },
+//     {
+//         "10th":"84%",
+//         "12th":"80"
+//     },
+//     {
+//         "M.E":"NO",
+//         "current":"access healthcare"
+//     },
+//     {
+//         "salary":"25k",
+//         "Location":"Bangalore"
+//     }
+// ]
+
+// console.log(resume);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
